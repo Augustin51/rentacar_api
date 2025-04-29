e94523b3c46388eabf2a8d3379061440e920b527
Eden <yacinelahjaily@gmail.com>
2025-04-29 17:12:29 +0200

    TECH: add route for vehicule

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Vehicle;
+use Illuminate\Support\Facades\DB;
+
 
 class VehicleController extends Controller {
     public function index() {
         $vehicles = Vehicle::with(['type', 'equipment', 'photo'])->get();
         return json_encode(['success' => true, 'vehicles' => $vehicles]);
+    }
+
+    public function detail($id) {
+
+        $test = Vehicle::with(['type', 'equipment', 'photo'])->where("id", $id)->get();
+        return json_encode(["vehicule" => $test]);
     }
 }
